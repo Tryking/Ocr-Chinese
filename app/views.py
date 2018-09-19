17e6000ed2886f03c003cf7fe2a893996219e145
dengkaiting <dengkaiting@migu.cn>
2018-09-19 13:02:02 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, render_template
 
 from app import app
 from app.image_ocr import handle_ocr
@@ -7,11 +7,22 @@ from app.libs.common import *
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'index'
+    return render_template('ocr.html')
 
 
 @app.route('/ocr', methods=['GET', 'POST'])
 def ocr():
+    result = request.form.to_dict()
+    version = get_result_param_value(result=result, param='version')
+    msgid = get_result_param_value(result=result, param='msgid')
+    systemtime = get_result_param_value(result=result, param='systemtime')
+    localUrl = get_result_param_value(result=result, param='localUrl')
+    handle_ocr(image_path=localUrl)
+    return 'true'
+
+
+@app.route('/ocr', methods=['GET'])
+def ocr_test_page():
     result = request.form.to_dict()
     version = get_result_param_value(result=result, param='version')
     msgid = get_result_param_value(result=result, param='msgid')
